a94275eab87989eb0f733013929e97e94718b79d
whitebeach03 <whitehama3@icloud.com>
2025-02-22 05:52:03 +0900

    create app

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Todo
 
-# Create your views here.
+# Todoリストを表示する
+def index(request):
+    todos = Todo.objects.all()
+    return render(request, "myapp/index.html", {"todos": todos})
+
+# 新しいTodoを追加する
+def add_todo(request):
+    pass
+    
+# Todoを完了済みにする
+
+
+
+# Todoを削除する
